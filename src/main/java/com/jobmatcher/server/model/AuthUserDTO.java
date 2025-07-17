5556b7aaaf13ff881cf5fd2d225e2632813e8477
MihaiS-git <ingsuciumihai@gmail.com>
2025-07-17 18:30:09 +0300

    add refresh token service tests

--- src/main/java/com/jobmatcher/server/model/AuthUserDTO.java
+++ src/main/java/com/jobmatcher/server/model/AuthUserDTO.java
@@ -1,12 +1,16 @@
 package com.jobmatcher.server.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthUserDTO {
 
     private UUID id;
